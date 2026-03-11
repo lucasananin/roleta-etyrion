e5b4b81524c25d5b86c4611c3192ea9dd1d37556
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2026-03-11 11:22:13 -0300

    feat: swipe handler added.

--- Roleta_URP/Assets/Scripts/Roulette/RouletteWheel.cs
+++ Roleta_URP/Assets/Scripts/Roulette/RouletteWheel.cs
@@ -4,90 +4,75 @@ using System.Collections;
 
 public class RouletteWheel : MonoBehaviour
 {
-    public RectTransform wheel;
-    public Button spinButton;
+    [SerializeField] RectTransform _wheel = null;
+    [SerializeField] Button _spinButton = null;
+    [SerializeField] int _numberOfSlots = 8;
+    [Space]
+    [SerializeField] float _minSpinTime = 10f;
+    [SerializeField] float _maxSpinTime = 15f;
+    [Space]
+    [SerializeField] float _minSpeed = 4f;
+    [SerializeField] float _maxSpeed = 6f;
 
-    public int numberOfSlots = 8;
+    private bool _spinning = false;
 
-    public float minSpinTime = 2f;
-    public float maxSpinTime = 4f;
-
-    public float minSpeed = 800f;
-    public float maxSpeed = 1200f;
-
-    bool spinning = false;
-
-    void Start()
+    private void Awake()
     {
-        spinButton.onClick.AddListener(Spin);
+        var _initialOffset = (360f / _numberOfSlots) / 2f;
+        _wheel.Rotate(0, 0, -_initialOffset);
+    }
+
+    private void OnEnable()
+    {
+        _spinButton.onClick.AddListener(Spin);
+    }
+
+    private void OnDisable()
+    {
+        _spinButton.onClick.RemoveListener(Spin);
     }
 
     public void Spin()
     {
-        if (!spinning)
+        if (!_spinning)
             StartCoroutine(SpinRoutine());
     }
 
-    IEnumerator SpinRoutine()
+    private IEnumerator SpinRoutine()
     {
-        spinning = true;
-        spinButton.interactable = false;
+        _spinning = true;
+        _spinButton.interactable = false;
 
-        float duration = Random.Range(minSpinTime, maxSpinTime);
-        float speed = Random.Range(minSpeed, maxSpeed);
+        float _duration = Random.Range(_minSpinTime, _maxSpinTime);
+        float _speed = Random.Range(GetSpin(_minSpeed), GetSpin(_maxSpeed));
 
-        float time = 0;
+        float _time = 0;
 
-        while (time < duration)
+        while (_time < _duration)
         {
-            float t = time / duration;
-
-            // desaceleração suave
-            float currentSpeed = Mathf.Lerp(speed, 0, t);
-
-            wheel.Rotate(0, 0, -currentSpeed * Time.deltaTime);
-
-            time += Time.deltaTime;
-
+            float _t = _time / _duration;
+            float _currentSpeed = Mathf.Lerp(_speed, 0, _t);
+            _wheel.Rotate(0, 0, -_currentSpeed * Time.deltaTime);
+            _time += Time.deltaTime;
             yield return null;
         }
 
-        //wheel.Rotate(0, 0, -5f);
-
         DetectSlot();
-
-        spinButton.interactable = true;
-        spinning = false;
+        _spinButton.interactable = true;
+        _spinning = false;
     }
 
-    void DetectSlot()
+    private void DetectSlot()
     {
-        float angle = wheel.eulerAngles.z;
-        float slotAngle = 360f / numberOfSlots;
-        int slot = Mathf.FloorToInt((angle % 360) / slotAngle);
-        int result = numberOfSlots - slot - 1;
-        //Debug.Log("Parou na casa: " + result);
-        Debug.Log($"Angle={angle} Result={result}");
+        float _angle = _wheel.eulerAngles.z;
+        float _slotAngle = 360f / _numberOfSlots;
+        int _slot = Mathf.FloorToInt((_angle % 360) / _slotAngle);
+        int _result = _numberOfSlots - _slot - 1;
+        Debug.Log($"Angle={_angle} Result={_result}");
+    }
 
-        //float finalAngle = (wheel.eulerAngles.z % 360) + UnityEngine.Random.Range(-10f, 10f); // Add randomness
-        //int segmentIndex = -1;
-        //float slotAngle = 360f / numberOfSlots;
-
-        //for (int i = 0; i < numberOfSlots; i++)
-        //{
-        //    //if (finalAngle >= segmentAngles[i] && finalAngle < segmentAngles[(i + 1) % numberOfSlots])
-        //    if (finalAngle >= slotAngle * i && finalAngle < slotAngle * (i + 1) % numberOfSlots)
-        //    {
-        //        segmentIndex = i;
-        //        break;
-        //    }
-        //}
-        ////if (segmentIndex == -1 && finalAngle >= segmentAngles[numberOfSlots - 1])
-        //if (segmentIndex == -1 && finalAngle >= numberOfSlots)
-        //{
-        //    segmentIndex = numberOfSlots - 1;
-        //}
-
-        //Debug.Log("Parou na casa: " + segmentIndex);
+    private float GetSpin(float _value)
+    {
+        return _value * 360;
     }
 }
---
+++ Roleta_URP/Assets/Scripts/Roulette/SwipeHandler.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SwipeHandler : MonoBehaviour
+{
+    private Vector2 startPos;
+    private float startTime;
+
+    public float minSwipeDistance = 50f; // Minimum pixels to count as swipe
+
+    void Update()
+    {
+        // TOUCH INPUT
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == UnityEngine.TouchPhase.Began)
+            {
+                startPos = touch.position;
+                startTime = Time.time;
+            }
+
+            if (touch.phase == UnityEngine.TouchPhase.Ended)
+            {
+                DetectSwipe(touch.position);
+            }
+        }
+
+        // MOUSE INPUT (for testing in editor)
+        //if (Input.GetMouseButtonDown(0))
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            startPos = Input.mousePosition;
+            startTime = Time.time;
+        }
+
+        //if (Input.GetMouseButtonUp(0))
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            DetectSwipe(Input.mousePosition);
+        }
+    }
+
+    void DetectSwipe(Vector2 endPos)
+    {
+        Vector2 delta = endPos - startPos;
+
+        // Only horizontal swipe
+        if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y) && Mathf.Abs(delta.x) > minSwipeDistance)
+        {
+            float swipeLength = Mathf.Abs(delta.x);
+            float swipeTime = Time.time - startTime;
+
+            //string direction = delta.x > 0 ? "Right" : "Left";
+
+            //Debug.Log($"Horizontal Swipe {direction}");
+            //Debug.Log($"Swipe Length: {swipeLength} pixels");
+            //Debug.Log($"Swipe Time: {swipeTime} seconds");
+
+            float swipeSpeed = swipeLength / swipeTime;
+            Debug.Log($"// swipe speed = {swipeSpeed}");
+        }
+    }
+}
