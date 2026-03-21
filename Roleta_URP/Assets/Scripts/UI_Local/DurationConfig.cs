46fc70a9ee2814bbf137156d1691bf59426be410
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2026-03-21 13:15:29 -0300

    feat: duration config added.

---
+++ Roleta_URP/Assets/Scripts/UI_Local/DurationConfig.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+
+public class DurationConfig : MonoBehaviour
+{
+    [SerializeField] GameDataSO _runtimeSO = null;
+    [SerializeField] TextMeshProUGUI _minTxt = null;
+    [SerializeField] TextMeshProUGUI _maxTxt = null;
+    [SerializeField] int _minDuration = 10;
+    [SerializeField] int _maxDuration = 30;
+
+    private void Awake()
+    {
+        UpdateVisuals();
+    }
+
+    public void IncreaseMin(int _value)
+    {
+        var _newValue = _runtimeSO.DurationRange.x + _value;
+        var _clampedValue = Mathf.Clamp(_newValue, _minDuration, _maxDuration);
+        _runtimeSO.DurationRange = new(_clampedValue, _runtimeSO.DurationRange.y);
+        UpdateVisuals();
+    }
+
+    public void IncreaseMax(int _value)
+    {
+        var _newValue = _runtimeSO.DurationRange.y + _value;
+        var _clampedValue = Mathf.Clamp(_newValue, _minDuration, _maxDuration);
+        _runtimeSO.DurationRange = new(_runtimeSO.DurationRange.x, _clampedValue);
+        UpdateVisuals();
+    }
+
+    public void UpdateVisuals()
+    {
+        _minTxt.text = $"{_runtimeSO.DurationRange.x}";
+        _maxTxt.text = $"{_runtimeSO.DurationRange.y}";
+    }
+}
