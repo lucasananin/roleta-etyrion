013a3ba0e19510d93b62f70b1275c4f8eab216a0
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2026-03-23 12:37:30 -0300

    min max speed config added.

---
+++ Roleta_URP/Assets/Scripts/UI_Local/SpeedConfig.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+
+public class SpeedConfig : MonoBehaviour
+{
+    [SerializeField] GameDataSO _runtimeSO = null;
+    [SerializeField] TextMeshProUGUI _minTxt = null;
+    [SerializeField] TextMeshProUGUI _maxTxt = null;
+    [SerializeField] int _minSpeed = 4;
+    [SerializeField] int _maxSpeed = 6;
+
+    private void Awake()
+    {
+        UpdateVisuals();
+    }
+
+    public void IncreaseMin(int _value)
+    {
+        var _newValue = _runtimeSO.SpeedRange.x + _value;
+        var _clampedValue = Mathf.Clamp(_newValue, _minSpeed, _maxSpeed);
+        _runtimeSO.SpeedRange = new(_clampedValue, _runtimeSO.SpeedRange.y);
+        UpdateVisuals();
+    }
+
+    public void IncreaseMax(int _value)
+    {
+        var _newValue = _runtimeSO.SpeedRange.y + _value;
+        var _clampedValue = Mathf.Clamp(_newValue, _minSpeed, _maxSpeed);
+        _runtimeSO.SpeedRange = new(_runtimeSO.SpeedRange.x, _clampedValue);
+        UpdateVisuals();
+    }
+
+    public void UpdateVisuals()
+    {
+        _minTxt.text = $"{_runtimeSO.SpeedRange.x}";
+        _maxTxt.text = $"{_runtimeSO.SpeedRange.y}";
+    }
+}
