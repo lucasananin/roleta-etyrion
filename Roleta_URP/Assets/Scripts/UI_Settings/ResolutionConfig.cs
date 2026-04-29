9940032d7f7caeac95cbed84235a8160bfd34a3d
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2026-04-29 17:18:13 -0300

    resolution settings added.

---
+++ Roleta_URP/Assets/Scripts/UI_Settings/ResolutionConfig.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionConfig : MonoBehaviour
+{
+    [Header("UI References")]
+    public TMP_Dropdown resolutionDropdown;
+    public Toggle fullscreenToggle; // optional
+
+    private Resolution[] resolutions;
+    private List<Resolution> uniqueResolutions = new List<Resolution>();
+
+    void Start()
+    {
+        InitializeResolutions();
+        //InitializeFullscreen();
+    }
+
+    void InitializeResolutions()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+        uniqueResolutions.Clear();
+
+        // Remove duplicate resolutions (same width/height)
+        uniqueResolutions = resolutions
+            .GroupBy(r => new { r.width, r.height })
+            .Select(g => g.First())
+            .OrderBy(r => r.width)
+            .ThenBy(r => r.height)
+            .ToList();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < uniqueResolutions.Count; i++)
+        {
+            Resolution res = uniqueResolutions[i];
+
+            string option = res.width + " x " + res.height;
+            options.Add(option);
+
+            if (res.width == Screen.currentResolution.width &&
+                res.height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+    }
+
+    void InitializeFullscreen()
+    {
+        if (fullscreenToggle != null)
+        {
+            fullscreenToggle.isOn = Screen.fullScreen;
+            fullscreenToggle.onValueChanged.AddListener(SetFullscreen);
+        }
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = uniqueResolutions[resolutionIndex];
+
+        Screen.SetResolution(
+            res.width,
+            res.height,
+            Screen.fullScreen
+        );
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
